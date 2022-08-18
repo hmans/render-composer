54a8251c07df381c01ab28b903a9d71267bda973
Hendrik Mans <hendrik@mans.de>
2022-08-18 08:53:14 +0200

    Make things more interesting

--- apps/examples/src/App.tsx
+++ apps/examples/src/App.tsx
@@ -1,18 +1,47 @@
 import { Environment, OrbitControls } from "@react-three/drei"
-import { RenderComposer } from "render-composer"
+import { Canvas, GroupProps, useFrame } from "@react-three/fiber"
+import { useRef } from "react"
+import { RenderComposer, RenderPipeline } from "render-composer"
+import { Group } from "three"
+
+const AutoRotate = (props: GroupProps) => {
+	const group = useRef<Group>(null!)
+
+	useFrame((_, dt) => {
+		group.current.rotation.x += dt * 0.7
+		group.current.rotation.y += dt * 0.5
+	})
+
+	return <group ref={group} {...props} />
+}
 
 function App() {
 	return (
-		<RenderComposer vignette bloom antiAliasing>
-			<color attach="background" args={["#264653"]} />
-			<Environment preset="sunset" />
-			<OrbitControls />
+		<Canvas
+			shadows
+			flat
+			gl={{
+				powerPreference: "high-performance",
+				alpha: false,
+				depth: true,
+				stencil: false,
+				antialias: false
+			}}
+		>
+			<RenderPipeline vignette bloom antiAliasing>
+				<color attach="background" args={["#264653"]} />
+				<Environment preset="sunset" />
 
-			<mesh>
-				<icosahedronGeometry />
-				<meshStandardMaterial color="#E9C46A" />
-			</mesh>
-		</RenderComposer>
+				<directionalLight position={[30, 10, 10]} intensity={1.5} />
+
+				<AutoRotate>
+					<mesh>
+						<icosahedronGeometry />
+						<meshStandardMaterial color="#E9C46A" metalness={0.5} roughness={0.5} />
+					</mesh>
+				</AutoRotate>
+			</RenderPipeline>
+		</Canvas>
 	)
 }
 
