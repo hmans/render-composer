3ce10286fc4dee772913dbc79dd9c0532b3b1c21
Hendrik Mans <hendrik@mans.de>
2022-08-18 08:39:19 +0200

    Set up simple example app

--- apps/examples/src/App.tsx
+++ apps/examples/src/App.tsx
@@ -1,5 +1,19 @@
+import { Environment, OrbitControls } from "@react-three/drei"
+import { RenderComposer } from "render-composer"
+
 function App() {
-	return <p>moo</p>
+	return (
+		<RenderComposer vignette bloom antiAliasing>
+			<color attach="background" args={["#264653"]} />
+			<Environment preset="sunset" />
+			<OrbitControls />
+
+			<mesh>
+				<icosahedronGeometry />
+				<meshStandardMaterial color="#E9C46A" />
+			</mesh>
+		</RenderComposer>
+	)
 }
 
 export default App
